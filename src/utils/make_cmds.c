262ee5955fc3b0e7bd6665957f4ec8a9774c616a
Z1nnlos <Noel.Sabia@gmx.net>
2024-02-08 11:34:44 +0100

    mergetime

--- src/utils/make_cmds.c
+++ src/utils/make_cmds.c
@@ -6,7 +6,7 @@
 /*   By: jkauker <jkauker@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 11:17:34 by jkauker           #+#    #+#             */
-/*   Updated: 2024/02/08 09:25:43 by jkauker          ###   ########.fr       */
+/*   Updated: 2024/02/08 10:34:27 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,12 +35,14 @@ int allocate_cmd(t_command *cmd, char **split)
 	arg_count = 0;
 	while (split[i] && is_operator(split[i]) == 0)
 	{
-		cmd->args[arg_count++] = ft_strdup(split[i++]);
-		if (!cmd->args[arg_count -1])
+		cmd->args[arg_count] = ft_strdup(split[i]);
+		if (!cmd->args[arg_count])
 			break;
+		arg_count++;
+		i++;
 		// printf("cmd->args[%d] = %s\n", arg_count, cmd->args[arg_count]);
 	}
-	printf("cmd->args[0] = %s\n", cmd->args[0]);
+	// printf("cmd->args[0] = %s\n", cmd->args[0]);
 	cmd->args[arg_count] = NULL;
 	if (split[i] && is_operator(split[i]) != 0)
 	{
@@ -54,6 +56,45 @@ int allocate_cmd(t_command *cmd, char **split)
 	cmd->next = NULL;
 	return (i);
 }
+
+// t_command *make_cmds(char *line, t_shell *shell)
+// {
+// 	char 		**split;
+// 	int			i;
+// 	t_command	*first, *current;
+
+// 	split = ft_split_shell(line);
+// 	if (!split)
+// 		return (0);
+// 	first = malloc(sizeof(t_command));
+// 	if (!first)
+// 		return (free_split(split));
+// 	i = allocate_cmd(first, split);
+// 	current = first;
+// 	while (split[i])
+// 	{
+// 		t_command *new_cmd = malloc(sizeof(t_command));
+// 		if (!new_cmd)
+// 		{
+// 			// handle error, free memory, etc.	
+// 			return (0);
+// 		}
+// 		i = allocate_cmd(new_cmd, &split[i]);
+// 		current->next = new_cmd;
+// 		new_cmd->prev = current;
+// 		current = new_cmd;
+// 	}
+// 	free_split(split);
+// 	return(first);
+// }
+
+
+
+
+
+
+
+
 
 t_command *make_cmds(char *line, t_shell *shell)
 {
