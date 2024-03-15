ec5fbf85e016798ef0253b731eac42e139b0322d
jkauker <jkauker@student.42heilbronn.de>
2024-03-15 11:00:39 +0100

    fixed some norm error in wildcard_match

--- src/utils/wildcard.c
+++ src/utils/wildcard.c
@@ -6,31 +6,29 @@
 /*   By: jkauker <jkauker@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 10:30:33 by jkauker           #+#    #+#             */
-/*   Updated: 2024/03/15 10:57:44 by jkauker          ###   ########.fr       */
+/*   Updated: 2024/03/15 11:00:15 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 #include <dirent.h>
 
-bool wildcard_match(char *pattern, char *str) {
-	if (*pattern == '\0' && *str == '\0') {
-		return true;
-	}
+bool	wildcard_match(char *pattern, char *str)
+{
+	if (*pattern == '\0' && *str == '\0')
+		return (true);
 
-	if (*pattern == '*' && *(pattern+1) != '\0' && *str == '\0') {
-		return false;
-	}
+	if (*pattern == '*' && *(pattern + 1) != '\0' && *str == '\0')
+		return (false);
 
-	if (*pattern == '?' || *pattern == *str) {
-		return wildcard_match(pattern+1, str+1);
-	}
+	if (*pattern == '?' || *pattern == *str)
+		return (wildcard_match(pattern + 1, str + 1));
 
-	if (*pattern == '*') {
-		return wildcard_match(pattern+1, str) || wildcard_match(pattern, str+1);
-	}
+	if (*pattern == '*')
+		return (wildcard_match(pattern + 1, str)
+			|| wildcard_match(pattern, str + 1));
 
-	return false;
+	return (false);
 }
 
 char	*get_matching_files(char *pattern)
