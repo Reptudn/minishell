e0615634822b375815066c49f190cbf3f93788e4
jkauker <jkauker@student.42heilbronn.de>
2024-03-25 12:45:38 +0100

    some multiple commands are working

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -3,15 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jkauker <jkauker@student.42.fr>            +#+  +:+       +#+        */
+/*   By: jkauker <jkauker@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 14:35:05 by jkauker           #+#    #+#             */
-/*   Updated: 2024/03/20 10:47:21 by jkauker          ###   ########.fr       */
+/*   Updated: 2024/03/25 12:32:28 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+// TODO: Isnt quite like the original bash export command
 int	ft_export(t_shell *shell, t_shunting_node *cmd)
 {
 	int			i;
