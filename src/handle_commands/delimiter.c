fc6c2eb7accc0b0fbe22e81979de13207015225b
Z1nnlos <Noel.Sabia@gmx.net>
2024-04-14 23:25:19 +0200

    the delimiter is nearly finished

--- src/handle_commands/delimiter.c
+++ src/handle_commands/delimiter.c
@@ -6,7 +6,7 @@
 /*   By: jkauker <jkauker@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 15:48:04 by jkauker           #+#    #+#             */
-/*   Updated: 2024/04/14 16:06:57 by jkauker          ###   ########.fr       */
+/*   Updated: 2024/04/14 21:24:52 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,36 @@
 
 void	print_cmd_chain(t_shunting_node **chain);
 
-char	*run_delimiter(t_shunting_node **chain)
+char	*run_delimiter(t_shunting_node **chain, t_shell *shell)
 {
 	char	*heredoc;
 	char	*new_heredoc;
 	char	*temp;
-	int		status;
+	int		exit_status;
 	int		counter;
+	int		pipefd[2];
+	char	*buffer = ft_calloc(100000, sizeof(char));
+	pid_t	pid;
+	int		status;
 
 	heredoc = malloc(sizeof(char) * 100);
 	heredoc[0] = '\0';
 	counter = 1;
-	print_cmd_chain(chain);
+	// print_cmd_chain(chain);
+	pipe(pipefd);
 	while (1)
 	{
 		temp = readline("heredoc> ");
 		if (!temp)
 		{
-			status = CMD_FAILURE;
+			exit_status = CMD_FAILURE;
 			break ;
 		}
 		if (chain[counter] && str_is_equal(temp, (chain[counter])->value))
 			counter++;
 		if (chain[counter] == NULL)
 		{
-			status = CMD_SUCCESS;
+			exit_status = CMD_SUCCESS;
 			free(temp);
 			break ;
 		}
@@ -46,9 +51,26 @@ char	*run_delimiter(t_shunting_node **chain)
 		free(heredoc);
 		heredoc = ft_strjoin(new_heredoc, "\n");
 		free(new_heredoc);
+		write(pipefd[1], temp, strlen(temp));
+		write(pipefd[1], "\n", 1);
 		free(temp);
 	}
-	*chain[0]->exit_status = status;
-	return (heredoc);
+	close(pipefd[1]);
+	pid = fork();
+	if (pid == 0)
+	{
+		dup2(pipefd[0], STDIN_FILENO);
+		exit_status = run_command(shell, chain[counter]);
+		close(pipefd[0]);
+		exit(exit_status);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		read(pipefd[0], buffer, sizeof(buffer));
+	}
+	*chain[0]->exit_status = exit_status;
+	printf("buffer: %s\n", buffer);
+	return (strdup(buffer));
 }
 // TODO: i think heredoc is not freed in the end
