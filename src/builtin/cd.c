a72e6e49c2e5c90584c9a62c38aee9cf17d6a51c
jkauker <jkauker@student.42heilbronn.de>
2024-04-15 16:21:35 +0200

    fixed cd

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -12,20 +12,46 @@
 
 #include "../../include/minishell.h"
 
+void	echo_err(char *new_path)
+{
+	ft_putstr_fd("cd: ", STDERR_FILENO);
+	if (!new_path)
+		ft_putstr_fd("HOME not set", STDERR_FILENO);
+	else
+		ft_putstr_fd(new_path, STDERR_FILENO);
+	if (errno == ENOENT)
+		ft_putstr_fd(": no such file or directory", STDERR_FILENO);
+	else if (errno == EACCES)
+		ft_putstr_fd(": permission denied", STDERR_FILENO);
+	else if (new_path)
+	{
+		ft_putstr_fd(": ", STDERR_FILENO);
+		ft_putstr_fd(strerror(errno), STDERR_FILENO);
+	}
+	ft_putstr_fd("\n", STDERR_FILENO);
+}
+
 //FIXME: cd should go back to root if no arguments are passed
 int	ft_cd(t_shunting_node *cmd, t_shell *shell)
 {
-	char	*new_path;
+	char		*new_path;
+	t_env_var	*tmp;
 
+	if (!cmd->args[0])
+	{
+		tmp = env_get_by_name(shell->env_vars, "HOME");
+		if (!tmp)
+			new_path = NULL;
+		else
+			new_path = tmp->value;
+	}
+	else
+		new_path = (cmd->args)[0];
 	if ((cmd->args)[1] != 0)
 		return (1);
-	new_path = (cmd->args)[0];
-	if (chdir(new_path) == -1)
+	if (!new_path || chdir(new_path) == -1)
 	{
-		ft_putstr_fd("cd: ", STDERR_FILENO);
-		ft_putstr_fd(new_path, STDERR_FILENO);
-		ft_putstr_fd(": ", STDERR_FILENO);
-		ft_putstr_fd(strerror(errno), STDERR_FILENO);
+		echo_err(new_path);
 		return (1);
 	}
 	free(shell->path);
