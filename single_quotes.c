0fefe3982f66c616974028799ee56ffd6ec0c546
Noel Sabia <128312469+NoelSabia@users.noreply.github.com>
2024-04-18 16:54:54 +0200

    just for single_quotes
    i didnt want to lose this cause things went south but this worked

---
+++ single_quotes.c
@@ -0,0 +1,25 @@
+void	single_quotes(const char *str, int i, int *start, char **result, int *res_i)
+{
+	char	quote;
+	int		len;
+
+	quote = str[i];
+	*start = i + 1;
+	i++;
+	while (str[i] && str[i] != quote)
+		i++;
+	if (str[i + 1] && str[i + 1] != ' ' && str[i + 1] != '\0')
+		i++;
+	else
+	{
+		len = i - *start;
+		if (len > 0)
+		{
+			result[*res_i] = create_split_string(str, *start, len);
+			(*res_i)++;
+		}
+		if (str[i] != '\0')
+			i++;
+		*start = i;
+	}
+}
