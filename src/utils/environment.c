2ca56b95debbf38dc3deb782699d038bb13b3670
Z1nnlos <Noel.Sabia@gmx.net>
2024-04-18 12:54:13 +0200

    some comments deleted

--- src/utils/environment.c
+++ src/utils/environment.c
@@ -112,9 +112,6 @@ t_env_var	*env_create_var(char *name, char *value, bool is_envp)
 	env_var->name = ft_strdup(name);
 	if (value)
 		env_var->value = ft_strdup(value);
-	// {
-	// 	free(value);
-	// }
 	else
 		env_var->value = NULL;
 	env_var->is_envp = is_envp;
