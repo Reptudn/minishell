8516af30084d7836831f48088af4b1a793ad8819
jkauker <jkauker@student.42heilbronn.de>
2024-04-25 10:01:48 +0200

    fixes to cd

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -27,6 +27,25 @@ void	echo_err(char *new_path)
 	ft_putstr_fd("\n", STDERR_FILENO);
 }
 
+int	set_pwd(t_shell *shell)
+{
+	t_env_var	*tmp;
+
+	tmp = env_get_by_name(shell->env_vars, "PWD");
+	if (tmp)
+	{
+		// if (tmp->value)
+		// 	free(tmp->value);
+		tmp->value = ft_strdup(getcwd(NULL, 0));
+		return (CMD_SUCCESS);
+	}
+	tmp = env_create_var("PWD", ft_strdup(getcwd(NULL, 0)), true);
+	if (!tmp)
+		return (CMD_FAILURE);
+	env_push(shell->env_vars, tmp);
+	return (CMD_SUCCESS);
+}
+
 int	ft_cd(t_shunting_node *cmd, t_shell *shell, char *new_path)
 {
 	t_env_var	*tmp;
@@ -35,6 +54,8 @@ int	ft_cd(t_shunting_node *cmd, t_shell *shell, char *new_path)
 		ft_putstr_fd("minishell: cd: too many arguments\n", STDERR_FILENO);
 	if (cmd->args[0] && cmd->args[1])
 		return (1);
+	if (str_is_equal(cmd->args[0], getcwd(NULL, 0)))
+		return (CMD_SUCCESS);
 	if (!cmd->args[0])
 	{
 		tmp = env_get_by_name(shell->env_vars, "HOME");
@@ -47,11 +68,12 @@ int	ft_cd(t_shunting_node *cmd, t_shell *shell, char *new_path)
 		new_path = (cmd->args)[0];
 	if ((cmd->args)[1] != 0)
 		return (1);
+	if (str_is_equal(cmd->args[0], "..") && str_is_equal(getcwd(NULL, 0), "/"))
+		return (CMD_SUCCESS);
 	if (!new_path || chdir(new_path) == -1)
 		echo_err(new_path);
 	if (!new_path || chdir(new_path) == -1)
 		return (0);
-	free(shell->path);
-	shell->path = getcwd(NULL, 0);
-	return (0);
+	// free(shell->path);
+	return (set_pwd(shell));
 }
--- src/fancy/prompt.c
+++ src/fancy/prompt.c
@@ -6,7 +6,7 @@
 /*   By: jkauker <jkauker@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/25 06:28:44 by jkauker           #+#    #+#             */
-/*   Updated: 2024/04/25 09:40:10 by jkauker          ###   ########.fr       */
+/*   Updated: 2024/04/25 10:01:00 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,23 @@ void	print_path(void)
 	t_prompt	prompt;
 	t_shell		*shell;
 	t_env_var	*path;
+	t_env_var	*term;
 
 	if (!isatty(fileno(stdin)))
 		return ;
-	prompt.term_type = env_get_by_name(get_shell()->env_vars, "TERM");
-	if (tgetent(prompt.term_buffer, prompt.term_type) != 1)
+	term = env_get_by_name(get_shell()->env_vars, "TERM");
+	if (!term || !term->value || tgetent(prompt.term_buffer, term->value) != 1)
 		return ;
 	path = env_get_by_name(get_shell()->env_vars, "PWD");
 	shell = get_shell();
+	printf("path value: %s\n", path->value);
 	prompt.col = tgetnum("co");
 	prompt.original_pos = tgetnum("cc");
-	prompt.position = prompt.col - ft_strlen(path);
+	prompt.position = prompt.col - ft_strlen(path->value);
 	prompt.move_cursor = tgoto(tgetstr("ch", NULL), 0, prompt.position);
 	tputs(prompt.move_cursor, 1, putchar);
 	tputs((char *)color_green(), 1, putchar);
-	if (path)
+	if (path && path->value)
 		tputs(path->value, 1, putchar);
 	tputs((char *)color_reset(), 1, putchar);
 	prompt.move_cursor = tgoto(tgetstr("ch", NULL), 0, prompt.original_pos);
