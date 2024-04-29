9b97abd3c05326d59ca99d05c388556471af65b0
jkauker <jkauker@student.42heilbronn.de>
2024-04-29 10:07:50 +0200

    fixed an issue where we dont check if the command is a builtin command just in captital case for example

--- src/handle_commands/run_single.c
+++ src/handle_commands/run_single.c
@@ -58,6 +58,38 @@ int	run_external_command(t_shell *shell, t_shunting_node *cmd)
 	return (status);
 }
 
+bool	is_invlid_builtin(char *cmd)
+{
+	char	*lower;
+	int		i;
+	bool	changed;
+	char	curr;
+
+	lower = ft_strdup(cmd);
+	if (!lower)
+		return (false);
+	i = -1;
+	changed = false;
+	while (lower[++i])
+	{
+		curr = lower[i];
+		lower[i] = ft_tolower(lower[i]);
+		if (curr != lower[i])
+			changed = true;
+	}
+	if (changed && (str_is_equal(lower, "history") || str_is_equal(lower, "exit")
+			|| str_is_equal(lower, "echo") || str_is_equal(lower, "pwd")
+			|| str_is_equal(lower, "clear") || str_is_equal(lower, "export")
+			|| str_is_equal(lower, "env") || str_is_equal(lower, "unset")
+			|| str_is_equal(lower, "cd")))
+		{
+			free(lower);
+			return (true);
+		}
+	free(lower);
+	return (false);
+}
+
 int	run_command(t_shell *shell, t_shunting_node *cmd)
 {
 	int	status;
@@ -66,7 +98,12 @@ int	run_command(t_shell *shell, t_shunting_node *cmd)
 		return (CMD_FAILURE);
 	replace_variable(&(cmd->value), cmd->args);
 	status = run_builtin_command(shell, cmd);
-	if (status == -1)
+	if (is_invlid_builtin(cmd->value))
+	{
+		print_invalid_cmd(cmd->value, "FAILURE", CMD_NOT_FOUND);
+		status = CMD_NOT_FOUND;
+	}
+	else if (status == -1)
 		status = run_external_command(shell, cmd);
 	if (cmd->exit_status == NULL)
 		cmd->exit_status = malloc(sizeof(int));
