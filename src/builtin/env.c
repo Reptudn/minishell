a25e0ecb26249c0cae9e23f5ea6c45675e42ee3f
jkauker <jkauker@student.42heilbronn.de>
2024-05-02 10:52:25 +0200

    reverted env

--- src/builtin/env.c
+++ src/builtin/env.c
@@ -26,28 +26,6 @@ int	print_env_vars(void)
 	return (CMD_SUCCESS);
 }
 
-// TODO: make this a bool return and return false if it didnt work and true if it works and just make a pointer ref to the vars var in the main env func
-t_env_var	*get_env_cmd_vars(char **args, int *i)
-{
-	t_env_var	*vars;
-	t_env_var	*new;
-	char		**split;
-
-	vars = NULL;
-	while (ft_strchr(args[++(*i)], '='))
-	{
-		split = split_first_occurence(args[*i], '=');
-		if (!split)
-			return (NULL);
-		new = env_create_var(split[0], split[1], false);
-		if (!new)
-			return (NULL);
-		env_push(vars, new);
-		free_split(split);
-	}
-	return (vars);
-}
-
 int	ft_env(t_shunting_node *cmd)
 {
 	t_env_var	*env_env;
@@ -55,10 +33,5 @@ int	ft_env(t_shunting_node *cmd)
 
 	if (!cmd)
 		return (CMD_FAILURE);
-	if (!cmd->args || !cmd->args[0])
-		return (print_env_vars());
-	i = -1;
-	env_env = get_env_cmd_vars(cmd->args, &i);
-
-	return (CMD_SUCCESS);
+	return (print_env_vars());
 }
