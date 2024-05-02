bba431c2f66e244406995f3e15d421a3286cf59c
jkauker <jkauker@student.42heilbronn.de>
2024-05-02 13:56:46 +0200

    fixed devcontainer

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,3 +1,4 @@
-echo "alias mstest='bash /workspaces/minishell/42_minishell_tester/tester.sh'" >> ~/.bashrc && bash
+echo "alias mstest='bash /workspaces/minishell/42_minishell_tester/tester.sh'" >> ~/.bashrc
 bash -c "$(curl -fsSL https://raw.github.com/xicodomingues/francinette/master/bin/install.sh)"
 echo 'Container created. Happy coding!'
+bash
