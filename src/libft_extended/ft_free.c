e8bfbef1999f65450e47579a3bcb407155be4876
jkauker <jkauker@student.42heilbronn.de>
2024-05-08 10:10:30 +0200

    cd leakfree

--- src/libft_extended/ft_free.c
+++ src/libft_extended/ft_free.c
@@ -6,7 +6,7 @@
 /*   By: jkauker <jkauker@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 08:22:09 by jkauker           #+#    #+#             */
-/*   Updated: 2024/05/07 08:23:39 by jkauker          ###   ########.fr       */
+/*   Updated: 2024/05/08 07:45:47 by jkauker          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,10 @@ void	*ft_free_many(void *return_val, int count, ...)
 	{
 		ptr = va_arg(args, void *);
 		if (ptr)
+		{
 			free(ptr);
+			ptr = NULL;
+		}
 	}
 	va_end(args);
 	return (return_val);
