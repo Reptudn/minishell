1c6aa7c40eca75fcfc2c01e62f632a6e4b6d5c65
Z1nnlos <Noel.Sabia@gmx.net>
2024-05-17 13:53:10 +0200

    delimiter and tab fix

--- src/handle_commands/delimiter_helper.c
+++ src/handle_commands/delimiter_helper.c
@@ -6,7 +6,7 @@
 /*   By: nsabia <nsabia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/12 15:28:07 by jkauker           #+#    #+#             */
-/*   Updated: 2024/05/17 12:27:32 by nsabia           ###   ########.fr       */
+/*   Updated: 2024/05/17 13:51:39 by nsabia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,30 @@ int	run_delmiter_im_crying(void)
 {
 	ft_putstr_fd("minishell: syntax error: unexpected end of file\n", 2);
 	return (1);
+}
+
+char	*get_input2(char *prompt)
+{
+	char	*line;
+	char	*tmp;
+
+	if (isatty(fileno(stdin)))
+		line = readline(prompt);
+	else
+	{
+		line = get_next_line(fileno(stdin));
+		if (!line)
+			return (NULL);
+		tmp = ft_strtrim(line, "\n");
+		ft_free(line);
+		line = tmp;
+	}
+	if (!line)
+		return (NULL);
+	tmp = ft_strtrim(line, " \t");
+	if (isatty(fileno(stdin)))
+		free(line);
+	else
+		ft_free(line);
+	return (tmp);
 }
